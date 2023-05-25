72a133b5ad41923ac52bdc1a9f91bde7719195cb
RichComingFromYou <s32200080@student.ubm.ac.id>
2023-05-25 22:59:00 +0700

    About and image link edit

--- views/about.py
+++ views/about.py
@@ -1,17 +1,21 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import urllib.request
 from PIL import Image
 
 def load_view():    
     st.title('About Page')
-    image = Image.open('D:\\Kerich\\UAS SEMES 6\\UAS WebDevPython\\Group-2-Web-Framework-Assignment\\assets\\Photo\\Logo Website.png')
+    urllib.request.urlretrieve('https://imagizer.imageshack.com/img924/9352/zxqpxz.png',"Logo Website.png")
+    image = Image.open("Logo Website.png")
     st.image(image, caption='Made By Vannes Cristian')
 
     st.divider()
 
     st.header("Company History")
-    image = Image.open('D:\\Kerich\\UAS SEMES 6\\UAS WebDevPython\\Group-2-Web-Framework-Assignment\\assets\\Photo\\Apollo.png')
+    urllib.request.urlretrieve('https://imagizer.imageshack.com/img922/3623/3k0lE8.jpg',"Apollo.jpg")
+    image = Image.open("Apollo.jpg")
+
     st.image(image, caption='The Greek God Apollo and his Solar Chariot')
     st.write("Apollo's Wheel originated from 4 young adults who dreamed of having their own start up company. The name 'Apollo's wheel' originated from the Greek God's himself Apollo, while the 'wheel' is referring to his famous Solar Chariot where he rode through the sky every day to bring light to the world. The idea is we hope that we as a company could also be the bringer of light, the bringer of good news, and the name itself sounds really cool")
     st.write("Our enthusiastic passion about cars drives us to make it more accessible for everyone to access any information regarding cars. whether physically, emotionally, or mentally, our desire to share our passion will never cease to end.")
