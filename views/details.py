fb0e304f0902754fec042fb380a447b16afd0708
Veegeedra <102303900+Veegeedra@users.noreply.github.com>
2023-05-26 18:34:28 +0900

    Modified Details Page
    Add another filter based on genmodel and line graph.

--- views/details.py
+++ views/details.py
@@ -5,28 +5,38 @@ import numpy as np
 def load_view():    
     st.title('Details Page')
 
-    # Fix image layout later
-    # st.image("https://www.kimballstock.com/images/car-stock-photos/cutout-car-images.jpg")
-
-    # st.write("Rp. 491.000.000,-")
-    # st.write("Kijang Innova Zenix")
-    # st.write("Honda")
-    # st.write("Gasoline 2.0 G CVT")
-    # st.write("M20A-FKS 4 cylinders, in-line 16-Valve DOHC, chain drive Dual VVT-i")
-
     @st.cache_data(ttl=600)
     def load_data(sheets_url: str):
         csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
         return pd.read_csv(csv_url)
 
-    # terus code dibawah ini tuh buat ngeload aja, bukan buat nampilin datanya
     df = load_data(st.secrets["price_table"])
 
     makers = []
     for row in df.itertuples():
         if(not makers.__contains__(row.Maker)):
             makers.append(row.Maker)
-    
+
     selectedMaker = st.selectbox("Filter by Maker:", makers)
-    table = df[df['Maker'] == selectedMaker]
-    st.table(table)
+    tablef1 = df[df['Maker'] == selectedMaker]
+
+    genmodels = []
+    for row in tablef1.itertuples():
+        if(not genmodels.__contains__(row.Genmodel)):
+            genmodels.append(row.Genmodel)
+
+    selectedGenmodel = st.selectbox("Filter by Genmodel:", genmodels)
+    tablef2 = tablef1[tablef1['Genmodel'] == selectedGenmodel]
+    st.table(tablef2)
+
+    tableDropped = tablef2.drop(['Maker', 'Genmodel', 'Genmodel_ID'], axis = 1)
+
+    year = []
+    entry_price = []
+    for row in tableDropped.itertuples():
+        year.append(row.Year)
+        entry_price.append(row.Entry_price)
+
+    chart_data = pd.DataFrame({selectedGenmodel : entry_price}, index = year)
+
+    st.line_chart(chart_data)
